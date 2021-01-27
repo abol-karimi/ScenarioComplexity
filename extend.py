4b284c0b3353a4d56d4acce55323539fb473bdae
Abolfazl Karimi <abolfazl.karimi@gmail.com>
2021-01-27 09:04:57 -0500

    Commandline maneuver argument: only incoming and outgoing lanes.

--- extend.py
+++ extend.py
@@ -7,19 +7,27 @@ import argparse
 parser = argparse.ArgumentParser(description='generate a new scenario.')
 parser.add_argument('inputfile', help='filename of the given scenario')
 parser.add_argument('outputfile', help='filename of the new scenario')
-parser.add_argument('-m', '--maneuver_uid', nargs='+', type=str,
+parser.add_argument('-m', '--from_to', nargs='+', type=str,
                     help='the maneuver of the nonego through the intersection')
 parser.add_argument('-s', '--nonego_spawn_distance', type=float, default=10.0,
                     help='initial distance of nonego to the intersection')
 args = parser.parse_args()
 
-if args.maneuver_uid:
-    nonego_maneuver_uid = tuple(args.maneuver_uid)
+with open(args.inputfile, 'rb') as inFile:
+    scenario = pickle.load(inFile)
+
+if args.from_to:
+    from scenic.domains.driving.roads import Network
+    network = Network.fromFile(scenario.map_path)
+    l0_uid = args.from_to[0]
+    l2_uid = args.from_to[1]
+    for m in network.elements[l0_uid].maneuvers:
+        if m.endLane.uid == l2_uid:
+            l1_uid = m.connectingLane.uid
+    nonego_maneuver_uid = (l0_uid, l1_uid, l2_uid)
 else:
     nonego_maneuver_uid = ('road9_lane2', 'road304_lane0', 'road45_lane1')
 
-with open(args.inputfile, 'rb') as inFile:
-    scenario = pickle.load(inFile)
 
 scenario = generator.extend(
     scenario,
--- new.py
+++ new.py
@@ -11,25 +11,41 @@ parser.add_argument('-i', '--intersection_uid', type=str,
                     help='the intersection at which the scenario happens')
 parser.add_argument('-r', '--rules_path',
                     help='the file containing the traffic rules')
-parser.add_argument('-e', '--ego_maneuver_uid', nargs='+', type=str,
+parser.add_argument('-e', '--ego_from_to', nargs='+', type=str,
                     help='the maneuver of the ego through the intersection')
-parser.add_argument('-n', '--nonego_maneuver_uid', nargs='+', type=str,
+parser.add_argument('-n', '--nonego_from_to', nargs='+', type=str,
                     help='the maneuver of the nonego through the intersection')
 parser.add_argument('-s', '--nonego_spawn_distance', type=float, default=10.0,
                     help='initial distance of nonego to the intersection')
 args = parser.parse_args()
 
 scenario = Scenario()
+
 if args.intersection_uid:
     scenario.intersection_uid = args.intersection_uid
 if args.rules_path:
     scenario.rules_path = args.rules_path
-if args.ego_maneuver_uid:
-    scenario.maneuver_uid['ego'] = tuple(args.ego_maneuver_uid)
+if args.ego_from_to or args.nonego_from_to:
+    from scenic.domains.driving.roads import Network
+    network = Network.fromFile(scenario.map_path)
+if args.ego_from_to:
+    l0_uid = args.ego_from_to[0]
+    l2_uid = args.ego_from_to[1]
+    for m in network.elements[l0_uid].maneuvers:
+        if m.endLane.uid == l2_uid:
+            l1_uid = m.connectingLane.uid
+    scenario.maneuver_uid['ego'] = (l0_uid, l1_uid, l2_uid)
+if args.nonego_from_to:
+    l0_uid = args.nonego_from_to[0]
+    l2_uid = args.nonego_from_to[1]
+    for m in network.elements[l0_uid].maneuvers:
+        if m.endLane.uid == l2_uid:
+            l1_uid = m.connectingLane.uid
+    nonego_maneuver_uid = (l0_uid, l1_uid, l2_uid)
 
 scenario = generator.extend(
     scenario,
-    nonego_maneuver_uid=tuple(args.nonego_maneuver_uid),
+    nonego_maneuver_uid=nonego_maneuver_uid,
     nonego_spawn_distance=args.nonego_spawn_distance)
 
 with open(args.outputfile, 'wb') as outFile:
